c24fda3b7b421615134591b0339301b86fccdc36
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-08-22 13:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+	let ans=0;
+	arr.sort((a,b)=>a-b);
+	while(arr.length>1){
+		let num1=arr.shift();
+		let num2=arr.shift();
+		let num=num1+num2;
+		ans+=num;
+		arr.push(num);
+		arr.sort((a,b)=>a-b);
+	}
+	return ans;
   
 }
 
